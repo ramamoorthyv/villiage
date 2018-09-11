b3705b4671fc1ff7d8316952f9c91759e4346cfe
Ramamoorthy Villithevar <Ramamoorthy.Villithevar@melbournewater.com.au>
2018-09-11 16:12:39 +1000

    change access modifier

--- villiage.go
+++ villiage.go
@@ -1,10 +1,10 @@
 // Package Villiage contains utility functions for working with age.
 package Villiage
 
-func find_age(yy int,mm int,dd int) int {
+import "time"
+
+func Find_age(yy int,mm int,dd int) int {
 	birth_day := time.Date(yy,time.Month(mm),dd,0,0,0,0,time.UTC) 		
 	age := time.Since(birth_day)
 	return int(( age.Hours() / 24 ) / 365)
 }
-
-
